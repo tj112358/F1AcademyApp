5137778eac20e7cda5a2881dd9b700dc5a190243
Theadora Yocum <theayocum@mac.com>
2025-09-12 11:26:49 -0400

    Initial Commit

---
+++ app2/app2App.swift
@@ -0,0 +1,17 @@
+//
+//  app2App.swift
+//  app2
+//
+//  Created by Thea Yocum on 9/12/25.
+//
+
+import SwiftUI
+
+@main
+struct app2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ app2Tests/app2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  app2Tests.swift
+//  app2Tests
+//
+//  Created by Thea Yocum on 9/12/25.
+//
+
+import Testing
+@testable import app2
+
+struct app2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
